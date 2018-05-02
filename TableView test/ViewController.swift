8623a2a609d45c56cc099687ca0bf0e5593f1974
Seo Sang won <33479927+zhaoSeo@users.noreply.github.com>
2018-05-02 14:48:15 +0900

    add alert

--- TableView test/ViewController.swift
+++ TableView test/ViewController.swift
@@ -33,6 +33,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let identifier = "RE"
         let cell = myTableView.dequeueReusableCell(withIdentifier: identifier, for: indexPath)
+        //다시 재활용.
         cell.textLabel?.text = data[indexPath.row]
         //title
         cell.detailTextLabel?.text = subData[indexPath.row]
@@ -41,6 +42,29 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         let myImg = UIImage(named:"image\(indexPath.row).jpeg");
         cell.imageView?.image = myImg
         return cell
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("section = \(indexPath.section) row = \(indexPath.row)")
+        print(data[indexPath.row])
+        print(subData[indexPath.row])
+        let myAlert = UIAlertController(title: data[indexPath.row], message: subData[indexPath.row], preferredStyle: UIAlertControllerStyle.alert)
+        let Action = UIAlertAction(title: "\(indexPath.section) Section \(indexPath.row) Row Selected", style: .default, handler: { (action: UIAlertAction) -> Void in
+        })
+        let okAction = UIAlertAction(title: "확인", style: .default, handler: { (action: UIAlertAction) -> Void in
+        })
+        
+        let kAction = UIAlertAction(title: "DELETE", style: .default, handler: { (action: UIAlertAction) -> Void in
+        })
+        //후행 클로져 let okAction = UIAlertAction(title: "종료", style: .default, {(action: UIAlertAction) -> Void in
+        let cancelAction = UIAlertAction(title: "CANCEL", style: .cancel, handler: { (action: UIAlertAction) -> Void in
+        })
+        
+        myAlert.addAction(Action)
+        myAlert.addAction(okAction)
+        myAlert.addAction(kAction)
+        myAlert.addAction(cancelAction)
+        
+        present(myAlert, animated: true, completion: nil)
     }
 }
 
